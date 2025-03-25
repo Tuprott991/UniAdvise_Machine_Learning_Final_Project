7c5686857dae4f98d99d0e0bdfb711e45b676c7d
Sang Nguyen <116291014+Sngyn04@users.noreply.github.com>
2025-03-25 15:14:54 +0700

    testtest

---
+++ Frontend/src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Chào mừng đến với EduAI</h1>
+            <p>Hệ thống tư vấn tuyển sinh thông minh.</p>
+        </div>
+    );
+}
+
+export default Homepage;
